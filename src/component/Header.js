4f7c1b1d56f78ea0d70ea400a580ea681d16f605
Kevin Annen <lekiyminin@gmail.com>
2020-07-06 09:01:33 -0500

    Created function for logo to resize on scroll

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,6 +7,44 @@ import {NavLink} from 'react-router-dom';
 class Header extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            logoSize: 13,
+            logoOffset: -10,
+            logoBorder: 5
+        }
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+
+    // This event resizes the logo in the navbar to follow the scrolling of the page.
+    handleScroll = (event) => {
+        const jumboH = document.getElementById("title").offsetHeight;
+        let scrollH = window.scrollY;
+        const logoSize = 13;
+        const logoOffset = -10;
+        const borderSize = 5;
+
+        if(scrollH > jumboH ) scrollH = jumboH;
+        let percent = Math.abs(scrollH-jumboH) / jumboH;
+
+        // if(percent > 1 ) percent = 1;
+        // console.log(percent);
+
+        const logoMin = 6;
+        this.setState({
+            logoSize: logoSize*(percent) < logoMin ? logoMin : logoSize*(percent),
+            logoOffset: logoOffset*percent,
+            logoBorder: borderSize*percent
+        })
     }
 
     render(){
@@ -34,9 +72,9 @@ class Header extends Component {
                            <NavLink className="nav-link" to="/contact">Contact Us</NavLink>
                         </NavItem>
                         <NavItem className="nav-item active">
-                            <NavLink className="nav-logo" to="/home">
-                                <img id="logo" src="/image/cherryblossomtreelogo.svg"/>
-                            </NavLink>
+                            {/* <NavLink className="nav-logo" to="/home"> */}
+                                <img id="logo" src="/image/cherryblossomtreelogo.svg" style={{border: `${this.state.logoBorder} px solid rgb(255, 236, 246)`, width:`${this.state.logoSize}rem`, top:`${this.state.logoOffset}rem`}}/>
+                            {/* </NavLink> */}
                         </NavItem>
                         <NavItem>
                             <NavLink className="nav-link" to="/products">Products</NavLink>
